f83b5cb4b0ece11b969016cfa29a9d14053bc495
LYNX <frankez478@gmail.com>
2023-08-23 12:45:16 +0000

    Commits

--- budget.js
+++ budget.js
@@ -1,23 +1,42 @@
+// function addExpense() {
+//   let exp = document.getElementById("expense");
+//   let exp1 = document.getElementById("expenseOne");
+//   // let btn = document.getElementById("btn");
+//   let add = document.getElementById("expenseList");
+//   // exp1.style.float = right;
+//   let listedExpense = document.createElement("li");
+//   let counter = 0;
+//   const maxInputs = 5;
+
+//   if (exp.value && exp1.value !== "") {
+//     listedExpense.innerText = `${exp.value}    ${exp1.value}naira`;
+//     add.appendChild(listedExpense);
+//     exp.value = "";
+//     exp1.value = "";
+//   }
+
+//   if (counter < maxInputs) {
+
+//   } else {
+
+//   }
+// }
+
+
 function addExpense() {
+
   let exp = document.getElementById("expense");
   let exp1 = document.getElementById("expenseOne");
-  // let btn = document.getElementById("btn");
-  let add = document.getElementById("expenseList");
-  // exp1.style.float = right;
   let listedExpense = document.createElement("li");
-  let counter = 0;
-  const maxInputs = 5;
-
+  let add = document.getElementById("orderlist");
   if (exp.value && exp1.value !== "") {
-    listedExpense.innerText = `${exp.value}    ${exp1.value}naira`;
+    listedExpense.innerText = `${exp.value}   ${exp1.value}Naira`;
     add.appendChild(listedExpense);
     exp.value = "";
     exp1.value = "";
   }
-
-  if (counter < maxInputs) {
-    
-  } else {
-    
-  }
 }
+
+// function removeExpense() {
+
+// }
